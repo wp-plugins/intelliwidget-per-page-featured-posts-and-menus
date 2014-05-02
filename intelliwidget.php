7b8608d358dc679d66547c0283e5afed5fe116b5
lilaeamedia <lilaeamedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-02 13:30:10 +0000

    Bug fix - incorrect object reference in class-intelliwidget-metabox.php affecting child profiles
    git-svn-id: https://plugins.svn.wordpress.org/intelliwidget-per-page-featured-posts-and-menus/tags/2.1.1@906874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- intelliwidget.php
+++ intelliwidget.php
@@ -6,7 +6,7 @@ if ( !defined('ABSPATH')) exit;
     Plugin Name: IntelliWidget Per Page Featured Posts and Menus
     Plugin URI: http://www.lilaeamedia.com/plugins/intelliwidget
     Description: Combine custom page menus, featured posts, sliders and other content into any widget area that can be customized on a per-page or site-wide basis.
-    Version: 2.1.0
+    Version: 2.1.1
     Author: Lilaea Media
     Author URI: http://www.lilaeamedia.com/
     Text Domain: intelliwidget
@@ -272,7 +272,7 @@ class IntelliWidget {
     
 }
 
-define('INTELLIWIDGET_VERSION', '2.1.0');
+define('INTELLIWIDGET_VERSION', '2.1.1');
 
 if (is_admin())
     include_once( 'includes/class-intelliwidget-admin.php' );
